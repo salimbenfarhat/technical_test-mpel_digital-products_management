f7330e8243caece0d14687d1e748a9c7d9aefcda
Salim Benfarhat <hello@salim.link>
2025-02-08 02:46:03 +0100

    🔧 Correction des erreurs ESLint dans app.controller.spec.ts et main.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return a welcome message', () => {
+      expect(appController.getWelcomeMessage()).toEqual({
+        message: 'Bienvenue sur l’API de gestion des produits ! 🚀',
+      });
     });
   });
 });
